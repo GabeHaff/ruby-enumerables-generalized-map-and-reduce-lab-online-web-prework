e75fc623c212ab78a4ca4d45fecc2465e3fa9898
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 14:37:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,18 @@ def map(array)
   new_array 
 end 
 
+reduce(source_array, start = nil)
+  if start
+    total = start
+    counter = 0
+  else
+    total = source_array[0]
+    counter= 1
+  end
+
+   while counter < source_array.length
+    total = yield(total, source_array[x])
+    counter += 1
+  end
+  total
+end 
